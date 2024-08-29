fa0ebf113e48c30b0dbd9f178c8c573df1f51efd
Юрков Тимофій Олександрович <145855201+Timofii0507@users.noreply.github.com>
2024-08-29 11:25:51 +0300

    task1

---
+++ hm-1.js
@@ -0,0 +1,8 @@
+const name = "Генератор";
+let price = 1000;
+
+console.log(`Обрано «${name}», ціна за одиницю ${price} кредитів.`);
+
+price = 2000;
+
+console.log(`Обрано «${name}», ціна за одиницю ${price} кредитів.`);
